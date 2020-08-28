f403614e218e927f69110639c0900a88647d1ec7
GM <gesundmeister@gms-imac.localdomain>
2020-08-28 08:15:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+# TODO
+# Averege area
+# Averege price
+# Most popular words in description
+# Pets allowed/no
+# How long it stayed alive (until ads got disabled)
+# Heatmap of prices/ locations to prove that city center is not more expensive
+# Averege rooms ?? area??
+# Area with most amount of immobilien offers
